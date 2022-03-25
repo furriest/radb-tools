127ac997bf6821bd0a338de44f54d07ec5eeda76
Valery Lutoshkin <valery@pluto.pw>
2022-03-25 18:10:15 +0300

    use local db

--- ip-country.py
+++ ip-country.py
@@ -6,6 +6,7 @@ import sys
 import subprocess
 import re
 import os
+import pyasn
 from aggregate_prefixes import aggregate_prefixes
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -13,6 +14,7 @@ url = 'https://ftp.ripe.net/ripe/asnames/asn.txt'
 networks = []
 pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})')
 filepath = os.path.dirname(sys.argv[0])
+asndb = pyasn.pyasn(filepath+'/ipasn.lst')
 
 try:
   country_code = sys.argv[1].upper()
@@ -25,10 +27,12 @@ result = filepath + '/ip_' + country_code + '.lst'
 response = requests.get(url, headers=headers).text.split('\n')
 asn = [ t.split(' ')[0] for t in response if t.split(' ')[-1] == country_code]
 with open(result, "w") as out_file:
-    for i in range(0, len(asn), 50):
-        output = subprocess.check_output('/usr/bin/bgpq3 -3A AS'+' AS'.join(asn[i:i + 50]), shell=True, text=True)
+    for i in range(0, len(asn), 1):
+#        output = subprocess.check_output('/usr/bin/bgpq3 -3A AS'+' AS'.join(asn[i:i + 50]), shell=True, text=True)
+        output = asndb.get_as_prefixes(asn[i])
         try:
-           networks.extend(re.findall(pattern,output))
+#           networks.extend(re.findall(pattern,output)) 
+            networks.extend(list(output))
         except:
            pass
         print(len(networks))
