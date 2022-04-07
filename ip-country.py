3fdd2e1582fc3fb5203fb1ef0454321514efe1e1
furriest <valery@pluto.pw>
2022-04-07 16:49:11 +0300

    minor fixes

--- ip-country.py
+++ ip-country.py
@@ -15,14 +15,14 @@ except:
 networks = []
 filepath = os.path.dirname(sys.argv[0])
 asndb = pyasn.pyasn(filepath+'/ipasn.lst')
-asnf = filepath + '/asn.txt'
+asnfile = filepath + '/asn.txt'
 result = filepath + '/ip_' + country_code + '.lst'
 
-with open(result, 'w') as out_file, open(asnf, 'r') as asn_file:
-    asn = [ t.split(' ')[0] for t in asn_file if t.split(' ')[-1][:2] == country_code]
-    for i in asn:
+with open(result, 'w') as out_file, open(asnfile, 'r') as asn_file:
+    asn_list = [ t.split(' ')[0] for t in asn_file if t.split(' ')[-1][:2] == country_code]
+    for asn in asn_list:
         try:
-            networks.extend(list(asndb.get_as_prefixes(i)))
+            networks.extend(list(asndb.get_as_prefixes(asn)))
         except:
             pass
     for line in list(aggregate_prefixes(networks)):
