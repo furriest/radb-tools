00c584389a7d252c1a7b363da5a1173976f5e469
furriest <valery@pluto.pw>
2022-04-10 01:33:27 +0300

    added ip-country-ripe.py

---
+++ ip-country-ripe.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# version: 0.4
+from ipaddress import IPv4Address, IPv4Network, ip_address, summarize_address_range
+import sys
+import os
+import json
+import requests
+from aggregate_prefixes import aggregate_prefixes
+
+try:
+  country_code = sys.argv[1].upper()
+except:
+  print('Usage: ', sys.argv[0], ' <two letters country code> ')
+  exit()
+
+networks = []
+filepath = os.path.dirname(sys.argv[0])
+result = filepath + '/ip_' + country_code + '.lst'
+url = 'https://stat.ripe.net/data/country-resource-list/data.json?resource='+country_code
+ripe_ip = json.loads(requests.get(url).content)['data']['resources']['ipv4']
+
+with open(result, 'w') as out_file:
+    for record in ripe_ip:
+#        try:
+            if record.find('-') > -1:
+                ips = record.split('-')
+                ipaddr = [ipa for ipa in summarize_address_range(IPv4Address(ips[0]),IPv4Address(ips[1]))]
+            else:
+                ipaddr = [IPv4Network(record)]
+            networks.extend(ipaddr)
+#        except:
+#            pass
+    for line in list(aggregate_prefixes(networks)):
+        print(str(line), file=out_file)
